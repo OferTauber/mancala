567f7829d7c6c05455df22b8b0efb59556d2ecb7
Ofer Tauber <ofertauber@gmail.com>
2022-07-06 20:11:22 +0300

    Skaleton

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const route = require('./src/route');
 const path = require('path');
 // require('./mongodb/mongoose'); // todo  - set mongo atlas
+const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(cors());
 app.use(express.text());
@@ -14,7 +18,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use('/', route);
 
-app.listen(PORT, (req, res) => {
+server.listen(PORT, (req, res) => {
   void req, res;
   console.log('Listen to port: ' + PORT);
 });
