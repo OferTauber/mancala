1d925e82e4d3a6d13bf31c88a79c803669121f45
Ofer Tauber <ofertauber@gmail.com>
2022-07-13 14:59:36 +0300

    MVP

--- client/src/contecst/socket_provider.jsx
+++ client/src/contecst/socket_provider.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import io from 'socket.io-client';
 
 const ENDPOINT = 'https://ofer-mancala.herokuapp.com/';
+// const ENDPOINT = 'http://127.0.0.1:5000/';
 // const ENDPOINT = process.env.PORT
 //   ? 'https://ofer-mancala.herokuapp.com/'
 //   : 'http://127.0.0.1:5000/';
