0928c901c971906e8b4201c053ad18fa5d63d4fd
Ofer Tauber <ofertauber@gmail.com>
2022-07-13 15:09:08 +0300

    Test - server

--- server/app.js
+++ server/app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-// const { convertPlayerMoveToOpponent } = require('./utils/convrtr_player_moves');
+const uniqid = require('uniqid');
 
 const port = process.env.PORT || 5000;
-// const index = require('./src/routes/index');
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -23,26 +22,71 @@ const io = new Server(server, {
   },
 });
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.emit('massage', 'Welcome');
+let globalWatingUser = undefined;
 
-  socket.on('ping', (ping) => {
-    io.emit('massage', ping);
+const handelNewUser = async (socket) => {
+  await sleep(200);
+  io.to(socket.id).emit('login', {
+    id: socket.id,
   });
+  await sleep(200);
+  if (globalWatingUser) startGame(socket, globalWatingUser);
+  else waitForOpponent(socket);
+};
+
+const startGame = async (socket1, socket2) => {
+  globalWatingUser = undefined;
+  console.log('wating room is empty');
+  firstPlayerId = randFirstPlayr(socket1, socket2);
+  const room = uniqid();
+  sleep(1200);
+
+  await socket1.join(room);
+  await socket2.join(room);
+
+  io.emit('game-start', {
+    room,
+    firstPlayerId,
+    players: [extractPlyerData(socket2), extractPlyerData(socket1)],
+  });
+
+  console.log('gameStart');
+};
+
+const waitForOpponent = (socket) => {
+  globalWatingUser = socket;
+  console.log(globalWatingUser.handshake.query.name + ' is wating');
+};
+
+const extractPlyerData = (socket) => {
+  return {
+    name: socket.handshake.query.name,
+    id: socket.id,
+  };
+};
+
+const randFirstPlayr = (socket1, socket2) => {
+  return Math.random() * 2 > 1 ? socket1.id : socket2.id;
+};
+
+io.on('connection', async (socket) => {
+  await socket.join('wating room');
+  console.log(socket.handshake.query.name + ' has connected');
+  handelNewUser(socket);
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log(socket.handshake.query.name + ' has disconnected');
+    if (globalWatingUser && socket.id === globalWatingUser.id)
+      globalWatingUser = undefined;
   });
 
-  socket.on('move', (pitNum) => {
-    // convertPlayerMoveToOpponent(moveResalt);
-    socket.broadcast.emit('opponent-move', pitNum);
-  });
-
-  socket.on('switch', () => {
-    socket.broadcast.emit('opponent-turn-end');
+  socket.on('move', ({ pitNum, room }) => {
+    socket.to(room).emit('opponent-move', pitNum);
   });
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
